bde05d94598857ada2d23a2259b0314fd8d66ca0
Alper Karaca <74460407+alpeerkaraca@users.noreply.github.com>
2021-11-03 16:15:56 +0300

    Added command for close the prgoram!
    If you type "!e" at search bar the program will close.

--- Tureng_Dictioanry_For_Linux.rb
+++ Tureng_Dictioanry_For_Linux.rb
@@ -1,33 +1,41 @@
 require 'launchy'
 
+
+
 puts "Please select a language.[ENTER will stop the program.]\t|Lütfen bir dil seçiniz.[Enter sonlandırır.]\n01=English\t\t\t\t\t\t|02=Turkish"
 selection = gets.chomp  
-loop do
+loop do 
     if selection == ("01" && "1")
-        puts "Please type the word you want to search."
+        puts "Please type the word you want to search.(Type !e ad close the program.)"
         words = gets.chomp
-        
-        puts ">>>>>>>>>>>>>>>>>Meaning of the word<<<<<<<<<<<<<<<".upcase
-        system "wikit #{words}"
-        Launchy.open("https://tureng.com/en/turkish-english/#{words}")
-        
-        
-    elsif selection == ("02" && "2")
-        puts "Lütfen aratmak istediğiniz sözcüğü yazın."
-        words = gets.chomp
-        
-        print ">>>>>>>>>>>>>>Kelimenin anlamı<<<<<<<<<<<<<<<<<<".upcase
-        system"wikit -l tr #{words}"        
-        Launchy.open("https://tureng.com/en/turkish-english/#{words}")
-        
-    else
-        return puts "[EN]You've made empty or wrong selection.Program is closing.[TR]Boş ya da yanlış seçim yaptınız. Program kapatılıyor."
-        break
-    end
-end
 
-        
+        if words == ("!e" && "!E")
+            break
+        elsif words.empty?
+            puts "You typed nothing.Program is closing."
+            break
+        else
+            puts ">>>>>>>>>>>>>>>>>Meaning of the word<<<<<<<<<<<<<<<".
+            system "wikit #{words}"
+            Launchy.open("https://tureng.com/en/turkish-english/#{words}")
+        end
     
 
 
+    elsif selection == ("02" && "2")
+        puts "Lütfen aratmak istediğiniz sözcüğü yazın.(Programı kapatmak için !e yazın.)"
+        words = gets.chomp
+        
+        if (words == "!e" or "!E"
+            break
+        end
 
+        if (words).empty?
+            puts "Hiçbir şey yazmadınız.Tekrar yazınız."
+        end
+    else
+        puts "[EN]You've made empty or wrong selection.Program is closing.[TR]Boş ya da yanlış seçim yaptınız. Program kapatılıyor."
+        break
+        
+    end
+end
